62bb144013a30ebafbbd026ccb4235c663336a1f
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-03-18 15:44:33 +0300

    Strings lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+const currentUser = 'Nicholas Mutisya';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+
+
+
+
+
+
+
+
+
+
+
